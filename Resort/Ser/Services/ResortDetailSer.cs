57727c1246ba88c6d015a7d00fb1c0a30469415d
Viet Nguyen <vietndph29972@fpt.edu.vn>
2023-12-20 21:14:07 +0700

    fix

--- Resort/Ser/Services/ResortDetailSer.cs
+++ Resort/Ser/Services/ResortDetailSer.cs
@@ -1,6 +1,29 @@
-﻿namespace Resort.Ser.Services
+﻿using Resort.Models;
+using Resort.Repo.IReponsitories;
+using Resort.Ser.IServices;
+
+namespace Resort.Ser.Services
 {
-	public class Class14
-	{
-	}
+    public class ResortDetailSer : IResortDetailSer
+    {
+        private readonly IResortDetailRepo _resortDetailRepo;
+        public ResortDetailSer(IResortDetailRepo resortDetailRepo)
+        {
+            _resortDetailRepo = resortDetailRepo;
+        }
+        public Task<bool> AddResortDetail(ResortDetail resortDetail)
+        {
+            return _resortDetailRepo.AddResortDetail(resortDetail);
+        }
+
+        public Task<ResortDetail> GetResortDetailByID(int id)
+        {
+            return _resortDetailRepo.GetResortDetailByID(id);
+        }
+
+        public Task<bool> UpdateResortDetail(ResortDetail resortDetail)
+        {
+            return _resortDetailRepo.UpdateResortDetail(resortDetail);
+        }
+    }
 }
