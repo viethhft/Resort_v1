706e126626222462ef2fdba5f44d677c3763ac88
Tbinh1505 <phungthanhbinh594@gmail.com>
2023-12-20 12:59:20 +0700

    form login done

--- Resort/Areas/Admin/Controllers/HomeAdminController.cs
+++ Resort/Areas/Admin/Controllers/HomeAdminController.cs
@@ -10,5 +10,11 @@ namespace Resort.Areas.Admin.Controllers
         {
             return View();
         }
+
+        [Route("Admin/Login")]
+        public IActionResult AdminLoginAccount()
+        {
+            return View();
+        }
     }
 }
