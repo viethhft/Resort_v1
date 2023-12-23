85c4446647421a2b77535ca0f9d09f4a8b66eb7f
Viet Nguyen <vietndph29972@fpt.edu.vn>
2023-12-23 15:31:39 +0700

    a

--- Resort/Areas/Admin/Controllers/AccountAdminController.cs
+++ Resort/Areas/Admin/Controllers/AccountAdminController.cs
@@ -3,37 +3,55 @@ using Newtonsoft.Json;
 using Resort.Models;
 using Resort.Repo.IReponsitories;
 using Resort.Ser.IServices;
+using Resort.Validate;
 
 namespace Resort.Areas.Admin.Controllers
 {
-    [Area("Admin")]
-    public class AccountAdminController : Controller
-    {
-        User user = null;
-        private readonly IUserSer _userSer;
-        private readonly IUserDetailSer _userDetailRepo;
-        public AccountAdminController(IUserSer userSer, IUserDetailSer userDetailSer)
-        {
-            _userSer = userSer;
-            _userDetailRepo = userDetailSer;
-        }
+	[Area("Admin")]
+	public class AccountAdminController : Controller
+	{
+		User user = null;
+		private readonly IUserSer _userSer;
+		private readonly IUserDetailSer _userDetailRepo;
+		public AccountAdminController(IUserSer userSer, IUserDetailSer userDetailSer)
+		{
+			_userSer = userSer;
+			_userDetailRepo = userDetailSer;
+		}
 
-        public async Task<IActionResult> Index(string email, string pass)
-        {
-            var user = await _userSer.GetUserByEmailAndPass(email, pass);
-            if (user != null)
-            {
-                ViewData["userDetail"] = await _userDetailRepo.GetUserDetailByIdUser(user.IdUser);
-                var userIn4 = JsonConvert.SerializeObject(await _userSer.GetUserById(user.IdUser));
-                var userDetailIn4 = JsonConvert.SerializeObject(ViewData["userDetail"]);
-                HttpContext.Session.SetString("user", userIn4);
-                HttpContext.Session.SetString("userDetail", userDetailIn4);
-                return RedirectToAction("IndexAdmin","HomeAdmin",user,"ok");
-            }
-            else
-            {
-                return View();
-            }
-        }
-    }
+		public async Task<IActionResult> Login(string email, string pass)
+		{
+			var user = await _userSer.GetUserByEmailAndPass(email, pass);
+			if (user != null)
+			{
+				ViewData["userDetail"] = await _userDetailRepo.GetUserDetailByIdUser(user.IdUser);
+				var userIn4 = JsonConvert.SerializeObject(await _userSer.GetUserById(user.IdUser));
+				var userDetailIn4 = JsonConvert.SerializeObject(ViewData["userDetail"]);
+				HttpContext.Session.SetString("user", userIn4);
+				HttpContext.Session.SetString("userDetail", userDetailIn4);
+				return RedirectToAction("IndexAdmin", "HomeAdmin");
+			}
+			else
+			{
+				return RedirectToAction("AdminLoginAccount", "HomeAdmin");
+			}
+		}
+
+		public async Task<IActionResult> Sign(string name, string email, string pass, string gender, string date)
+		{
+			if (name != null && email != null && pass != null && gender != null && date != null)
+			{
+				User user = new User() { Email = email, Password = ValidateRegex.ReversePass(pass), IdRole = 2, Status = 1 };
+				await _userSer.AddUser(user);
+				user=await _userSer.GetUserByEmailAndPass(email,pass);
+				UserDetail userDetail = new UserDetail() { IdUser = user.IdUser, Description = "", Gender = gender == "Nam" ? 0 : 1, DoB = date, UserName = name, ImageProfile = gender == "Nam" ? "6.jpg" : "7.jpg" };
+				await _userDetailRepo.AddUserDetail(userDetail);
+				return RedirectToAction("AdminLoginAccount", "HomeAdmin");
+			}
+			else
+			{
+				return RedirectToAction("AdminSignAccount", "HomeAdmin");
+			}
+		}
+	}
 }
---
+++ Resort/Validate/ValidateRegex.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Resort.Validate
+{
+	public class ValidateRegex
+	{
+		public static string ReversePass(string password)
+		{
+			byte[] pass_byte = ASCIIEncoding.ASCII.GetBytes(password);//dịch pass ra dạng bytes theo bảng mã ASCII
+			MD5 mD5 = MD5.Create();
+
+			var haspass = mD5.ComputeHash(pass_byte);//chuyển mảng bytes vừa dịch ra thành mảng bytes khác bằng md5
+			string savehas = "";
+
+			foreach (var item in haspass)
+			{
+				//chuyển dạng bytes về chuỗi để đảo ngược dãy bytes
+				savehas += item;
+			}
+
+			char[] term = savehas.ToCharArray();
+			Array.Reverse(term);//hàm đảo ngược
+			string new_haspasscode = new string(term);// đưa về dạng chuỗi sau khi đảo
+
+			pass_byte = ASCIIEncoding.ASCII.GetBytes(new_haspasscode);//tiếp tục chuyển sang bytes
+			haspass = mD5.ComputeHash(pass_byte);//tiếp tục dùng md5 phân nhỏ
+			savehas = "";
+			foreach (var item in haspass)
+			{
+				savehas += item;
+			}
+
+			char[] chars = savehas.ToCharArray();//đưa về mảng char
+												 //mảng char theo ascii công thức: char+ vị trí chính nó + char vị trí liền kề %2
+			var char_reverse = chars.Select((value, index) => new { value, index }).ToArray();
+			var finish = char_reverse.Select(c => c.value + c.index + (char_reverse.Length > c.index + 1 ? char_reverse[c.index + 1].value % 2 : char_reverse[c.index / 2].value % 2)).Select(c => (char)c).ToArray();
+			string new_char = new string(finish);
+			foreach (var item in char_reverse)//đưa về dạng chuỗi trả ra kết quả
+			{
+				new_char += item.value;
+			}
+
+			return new_char;
+		}
+
+	}
+}
